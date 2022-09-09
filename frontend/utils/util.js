cdf75c26d29667f2c4dd358b76985046804d0124
Lalit <lalitkumarsingh3716@gmail.com>
2022-09-09 08:15:03 +0530

    Configured Eslint and Prettier for consistent Code Quality

--- frontend/utils/util.js
+++ frontend/utils/util.js
@@ -1,1 +1,1 @@
-//every helper function that optimize the codebase or resusable throughtout the project will come in utils.
+// every helper function that optimize the codebase or resusable throughtout the project will come in utils.
